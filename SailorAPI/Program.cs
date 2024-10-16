caa8b82db45512ab2b41106a3d7d6709eb535fa6
Md. Faisal Rony <faisal.rony@epylliongroup.com>
2024-10-16 09:00:55 +0600

    WebAPI

--- SailorAPI/Program.cs
+++ SailorAPI/Program.cs
@@ -1,5 +1,7 @@
 using Sailor.Application.Interface;
 using Sailor.Infrastructure.Service.SCM;
+using Sailor.Repository.Implementation.SCM;
+using Sailor.Repository.Interface.SCM;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IFabricPoService, FabricPoService>();
+builder.Services.AddScoped<IFabricPoRepository, FabricPoRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
