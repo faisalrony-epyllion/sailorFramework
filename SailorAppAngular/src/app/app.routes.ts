60b5ae5a9161f8a08693b86a59fd9369994a877e
Md. Faisal Rony <faisal.rony@epylliongroup.com>
2024-10-27 17:16:53 +0600

    module addded

--- SailorAppAngular/src/app/app.routes.ts
+++ SailorAppAngular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
  
 import { BodyComponent } from './shared/layouts/body/body.component';
+import { FabricComponent } from './scm/fabric/fabric.component';
+import { AccessoriesComponent } from './scm/accessories/accessories.component';
  
 
  
@@ -8,7 +10,10 @@ import { BodyComponent } from './shared/layouts/body/body.component';
 
   export const routes: Routes = [  
    
-    { path: '', component: BodyComponent },];
+    { path: '', component: BodyComponent },
+    { path: 'fabric', component: FabricComponent },
+    { path: 'accessories', component: AccessoriesComponent },
+  ];
 
 
     
