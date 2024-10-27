b65ec5fb751b9821cd63e39fcaf44b3666ed4218
Md. Faisal Rony <faisal.rony@epylliongroup.com>
2024-10-27 17:02:00 +0600

    Layout Admin LTE

--- SailorAppAngular/src/app/app.component.ts
+++ SailorAppAngular/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/layouts/header/header.component";
+import { FooterComponent } from "./shared/layouts/footer/footer.component";
+import { SidebarComponent } from './shared/layouts/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, HeaderComponent, FooterComponent,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- SailorAppAngular/src/app/app.routes.ts
+++ SailorAppAngular/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+ 
+import { BodyComponent } from './shared/layouts/body/body.component';
+ 
 
-export const routes: Routes = [];
+ 
+      
+
+  export const routes: Routes = [  
+   
+    { path: '', component: BodyComponent },];
+
+
+    
