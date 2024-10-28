fd7547d5def96408a4e7de0505a9dfcc3cf88bc8
ShiafRahman19188 <shiafur.rahman@epylliongroup.com>
2024-10-28 11:03:21 +0600

    commit"SCM SubModules"

---
+++ SailorAppAngular/src/app/scm/acknowledge-pr/acknowledge-pr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acknowledge-pr',
+  standalone: true,
+  imports: [],
+  templateUrl: './acknowledge-pr.component.html',
+  styleUrl: './acknowledge-pr.component.css'
+})
+export class AcknowledgePRComponent {
+
+}
---
+++ SailorAppAngular/src/app/scm/open-po-approval/open-po-approval.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-po-approval',
+  standalone: true,
+  imports: [],
+  templateUrl: './open-po-approval.component.html',
+  styleUrl: './open-po-approval.component.css'
+})
+export class OpenPoApprovalComponent {
+
+}
