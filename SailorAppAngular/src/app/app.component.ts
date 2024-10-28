de8fdd013978073f80dfffe2bffea27bcd2f0593
Md. Faisal Rony <faisal.rony@epylliongroup.com>
2024-10-28 08:36:24 +0600

    seperate the route for modules

--- SailorAppAngular/src/app/app.component.ts
+++ SailorAppAngular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import {  RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/layouts/header/header.component";
 import { FooterComponent } from "./shared/layouts/footer/footer.component";
 import { SidebarComponent } from './shared/layouts/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent,SidebarComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent,SidebarComponent ,CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- SailorAppAngular/src/app/app.routes.ts
+++ SailorAppAngular/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
  
 import { BodyComponent } from './shared/layouts/body/body.component';
-import { FabricComponent } from './scm/fabric/fabric.component';
-import { AccessoriesComponent } from './scm/accessories/accessories.component';
+import { scmroutes } from './scm/scm.route';
+ 
  
 
  
@@ -11,8 +11,7 @@ import { AccessoriesComponent } from './scm/accessories/accessories.component';
   export const routes: Routes = [  
    
     { path: '', component: BodyComponent },
-    { path: 'fabric', component: FabricComponent },
-    { path: 'accessories', component: AccessoriesComponent },
+    ...scmroutes
   ];
 
 
