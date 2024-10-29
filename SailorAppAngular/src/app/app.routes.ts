3e0abf3797146ac632dbeb74edaa383c1122ab8c
Md. Faisal Rony <faisal.rony@epylliongroup.com>
2024-10-29 09:15:49 +0600

    Remove Unused Files

--- SailorAppAngular/src/app/app.routes.ts
+++ SailorAppAngular/src/app/app.routes.ts
@@ -1,15 +1,5 @@
 import { Routes } from '@angular/router';
- 
-import { BodyComponent } from './shared/layouts/body/body.component';
-import { scmroutes } from './scm/scm.route';
-import { userroutes } from './user/user.route';
-import { businessPlanroutes } from './businessPlan/businessPlan.route';
-import { rangePlanroutes } from './rangePlan/rangePlan.route';
-import { planningAndAllocationroutes } from './planningAndAllocation/planningAndAllocation.route';
-import { designManagementroutes } from './designManagement/designManagement.route';
-import { merchandisingManagementroutes } from './merchandisingMgt/merchandisingMgt.route';
-import { mcdroutes } from './mcd/mcd.route';
- 
+
  
 
  
